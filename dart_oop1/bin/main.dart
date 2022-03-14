0fdfcaf0542d00ce292821a6f52e0f7aa50689d0
Pujidhiyanabila <nabilapuji26@gmail.com>
2022-03-14 17:06:06 +0700

    update pratikum 9

--- dart_oop1/bin/main.dart
+++ dart_oop1/bin/main.dart
@@ -1,14 +1,24 @@
-import 'dart:io';
-
 import 'hewan.dart';
 import 'mobil.dart';
-void main(){
-  var mobil = Mobil();
-  stdout.write("Inputkan kapasitas mobil : ");
-  mobil.kapasitas = int.parse(stdin.readLineSync().toString());
-  print("Kapasitas maksimum mobil adalah ${mobil.kapasitas} Kg");
-  
-  mobil.tambahMuatan(Hewan());
-  mobil.totalMuatan(Hewan());
-}
 
+void main(List<String> arguments) {
+  Hewan kambing = Hewan();
+  kambing.berat = 50;
+
+  Hewan sapi = Hewan();
+  sapi.berat = 100;
+
+  Hewan ayam = Hewan();
+  ayam.berat = 10;
+
+  Mobil mobilPengangkut = Mobil();
+  mobilPengangkut.kapasitas = 3;
+
+  if (mobilPengangkut.kapasitas! > 1) {
+    mobilPengangkut.tambahMuatan(kambing);
+    mobilPengangkut.tambahMuatan(sapi);
+    mobilPengangkut.tambahMuatan(ayam);
+  }
+
+  mobilPengangkut.totalMuatan();
+}
--- dart_oop1/bin/mobil.dart
+++ dart_oop1/bin/mobil.dart
@@ -1,42 +1,14 @@
-import 'dart:io';
-
 import 'hewan.dart';
 
 class Mobil {
   int? kapasitas;
+  List<Hewan> muatan = [];
 
-  List<Hewan>? muatan = [];
+  void tambahMuatan(Hewan hewan) {
+    muatan.add(hewan);
+  }
 
-  void tambahMuatan(Hewan hewan){
-  Mobil mobil = new Mobil();
-  int i = 0;
-  bool ulang = true;
-
-  do {
-    var hewan = Hewan();
-    stdout.write("Inputkan berat hewan : ");
-    hewan.berat = int.parse(stdin.readLineSync().toString());
-    print("Berat hewan adalah ${hewan.berat} Kg");
-    
-    List<Hewan> newMuatan = [];
-    newMuatan.add(hewan);
-    muatan = newMuatan;
-    print(muatan);
-
-    stdout.write("Apakah anda ingin menambah muatan (y/t) : ");
-    String? jawaban = stdin.readLineSync();
-    
-    i++;
-    if (jawaban.toString().toUpperCase() == "T"){
-      ulang = false;
-    }
-    } while (ulang);
-
-    print("Total muatan yang telah ditambahkan sebanyak : $i");
-    }
-
-  void totalMuatan(Hewan hewan){
-    Mobil mobil = new Mobil();
-
+  void totalMuatan() {
+    print(muatan.length);
   }
 }
